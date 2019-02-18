0e40bcc4c5ca82fddbc4208e47a569e11e7c9d7a
ZhangZisu <admin@zhangzisu.cn>
2019-02-18 19:43:27 +0800

    Update

--- client/src/web.ts
+++ client/src/web.ts
@@ -1,6 +1,13 @@
 import express = require("express");
+import { join } from "path";
 import { api } from "./api";
 
 export const app = express();
 
 app.use("/api", api);
+
+const UIRoot = join(__dirname, "..", "web", "dist");
+app.use(express.static(UIRoot));
+app.get("*", (req, res) => {
+    res.sendFile(join(UIRoot, "index.html"));
+});
