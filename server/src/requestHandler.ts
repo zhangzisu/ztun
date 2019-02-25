903a1c84de1f86e549dd306d30d6e4aff70d8f20
ZhangZisu <admin@zhangzisu.cn>
2019-02-25 22:14:34 +0800

    Update

--- server/src/requestHandler.ts
+++ server/src/requestHandler.ts
@@ -1,3 +1,9 @@
 import connect = require("connect");
 
 export const requestHandler = connect();
+
+requestHandler.use((req: any, res: any, next: any) => {
+    res.setHeader("X-Powered-By", "ztun-connect");
+    res.setHeader("Server", "ztun-node-http");
+    return next();
+});
